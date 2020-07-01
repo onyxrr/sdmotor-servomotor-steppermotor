4241bd75896cd0aaec9c55ffd6ebc827cb953e8b
onyxrr <67698705+onyxrr@users.noreply.github.com>
2020-07-01 18:11:05 +0300

    Add files via upload

---
+++ task_one_-dc_motor/task_one_-dc_motor.ino
@@ -0,0 +1,15 @@
+void setup()
+{
+  pinMode(10, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(10, HIGH);
+  digitalWrite(12, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(10, LOW);
+  digitalWrite(12, LOW);
+  delay(4000); // Wait for 4000 millisecond(s)
+}
